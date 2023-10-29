1d18b369e7648e80e8eda6d051c051bc18a6045b
nithyavarshini <nithyavarshini23@gmail.com>
2023-10-29 14:24:23 +0530

    nithya commit1

--- watermarkremover.py
+++ watermarkremover.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-
+anitta=25
 def watermark_photo(input_image_path,watermark_image_path,output_image_path):
     base_image = Image.open(input_image_path)
     watermark = Image.open(watermark_image_path).convert("RGBA")
